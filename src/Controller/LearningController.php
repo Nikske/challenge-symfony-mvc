024d822ce30eb9f613dfa59ff40c9d34ad3f3a7f
Nikske <wolfvdd@gmail.com>
2020-03-03 15:49:07 +0100

    cleaning things up and redirecting when there's an empty post

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -7,40 +7,37 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LearningController extends AbstractController {
     private $name;
+    private function getName() {
+        return $this->name;
+    }
 
     /**
      * @Route("/about-me", name="aboutMe")
      */
     public function aboutMe() {
         return $this->render('learning/index.html.twig', [
-            'controller_name' => 'LearningController',
         ]);
-    }
-    private function getName() {
-        return $this->name;
     }
 
     /**
      * @Route("/show-name", name="showName")
      */
     public function showMyName() {
-        if (!isset($_POST['myName'])) {
-            $_POST['myName']= "undefined";
+        if (empty($_POST['myName'])){
+            return $this->redirectToRoute('changeName');
+        } else {
+            $this->name = $_POST['myName'];
+            return $this->render('learning/showname.html.twig', [
+                'name' => $this->getName()
+            ]);
         }
-        $this->name = $_POST['myName'];
-        return $this->render('learning/showname.html.twig', [
-            'controller_name' => 'LearningController',
-            'name' => $this->getName()
-        ]);
+
     }
 
     /**
      * @Route("/", name="changeName")
      */
     public function changeMyName() {
-        return $this->render('learning/changename.html.twig', [
-            'controller_name' => 'LearningController',
-            'name' => $this->getName()
-        ]);
+        return $this->render('learning/changename.html.twig');
     }
 }
